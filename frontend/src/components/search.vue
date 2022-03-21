39d39a0582c3a4d34557be8e9bd62d5b5431cc3e
D. Neykov <metheorita@gmail.com>
2022-03-21 03:08:22 +0100

    Minor formatting fix

--- frontend/src/components/search.vue
+++ frontend/src/components/search.vue
@@ -9,31 +9,29 @@
 </template>
 
 <script>
-import { ref } from 'vue'
-import data from '../assets/testsong.js'
 import SongCard from "./songcard.vue";
 import axios from "axios";
 
-  export default {
-    name: "Search",
-    components: { SongCard },
-    data() {
-        let songs = []
-        return {
-           songs
-        }
-    },
-    methods: {
-        searchForFileName() {
-            this.songs.length = 0 // clear the array while making sure that reference isn't lost
-            let searchRequest = { value: this.searchValue}
-            axios.post("http://backend-starstream.localhost:8000/search/", searchRequest).then(response => {
-              response.data.results.forEach(songId => {
-                axios.get("http://backend-starstream.localhost:8000/getmetadata/?id=" + songId).then(response => (this.songs.push(response.data)))
-              })
+export default {
+  name: "Search",
+  components: { SongCard },
+  data() {
+      let songs = []
+      return {
+          songs
+      }
+  },
+  methods: {
+      searchForFileName() {
+          this.songs.length = 0 // clear the array while making sure that reference isn't lost
+          let searchRequest = { value: this.searchValue}
+          axios.post("http://backend-starstream.localhost:8000/search/", searchRequest).then(response => {
+            response.data.results.forEach(songId => {
+              axios.get("http://backend-starstream.localhost:8000/getmetadata/?id=" + songId).then(response => (this.songs.push(response.data)))
             })
-        }
-    }
+          })
+      }
+  }
 }
 </script>
 
